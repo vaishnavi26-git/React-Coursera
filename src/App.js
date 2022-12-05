ef9d337da280070299c0508222c76e110ec237a3
Vaishnavi Kumari <vaishnavikumari26@gmail.com>
2022-12-05 12:02:36 +0530

    header n footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 // import { Navbar, NavbarBrand } from 'reactstrap';
 import Main from "./components/MainComponent"
+import "./App.css"
 // import {DISHES} from "./shared/dishes"
 class App extends Component {
   // constructor(props) {
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+// import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from "./MenuComponent"
 import {DISHES} from "../shared/dishes"
+import Header from './HeaderComponent';
 import DishDetail from './DishdetailComponent';
+import Footer from './FooterComponent';
 
 class Main extends Component {
   constructor(props) {
@@ -24,13 +26,15 @@ class Main extends Component {
   render() {
     return (
       <div>
-                <Navbar dark color="primary">
+        <Header/>
+                {/* <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
-        </Navbar>
+        </Navbar> */}
         <Menu dishes={this.state.dishes} onClick={ (dishId) => this.onDishSelect( dishId )} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+              <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+              <Footer/>
       </div>
     );
   }
