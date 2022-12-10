5e22e9fa5865dbc373696470708c1699351290e9
Vaishnavi Kumari <vaishnavikumari26@gmail.com>
2022-12-10 15:57:26 +0530

    redux action

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -9,6 +9,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 
 
 
+
 /**........................ comment component ends ................................................. */
 //// validators
 const required = (val) => val && val.length; //value > 0
@@ -31,11 +32,10 @@ class CommentForm extends Component {
     }
 
     handleCommentFormSubmit(values) {
-        console.log("Current State is: " + JSON.stringify(values));
+        // console.log("Current State is: " + JSON.stringify(values));
         // alert("Current State is: " + JSON.stringify(values));
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
 
-        // action obj
-        this.props.postComment( this.props.dishId, values.rating, values.author, values.comment );
 
     }
 
@@ -175,16 +175,13 @@ class CommentForm extends Component {
         if (dish != null) {
             return (
                 <div className='col-12 col-md-5 m-1'>
-
                     <Card>
-                        <CardImg width="100%" src={ dish.image} alt={dish.name} />
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardBody>
                             <CardTitle> {dish.name}</CardTitle>
                             <CardText> {dish.description} </CardText>
                         </CardBody>
                     </Card>
-
-
                 </div>   
             );
         }
@@ -195,7 +192,7 @@ class CommentForm extends Component {
         }
     }
 
-    function RenderComments({ dish, comments, postComment, dishId }){
+    function RenderComments({comments, addComment, dishId}) {
         if (comments == null) {
             return (<div></div>)
         }
@@ -222,22 +219,7 @@ class CommentForm extends Component {
                 <ul className='list-unstyled'>
                     {cmnts}
                 </ul>
-
-                    {comments.map((comment) => {
-                        return (
-                                <li key={comment.id}>
-                                    <p>{comment.comment}</p>
-                                    <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
-                                </li>
-                        );
-                    })}
-
-                <CommentForm 
-                    dish={dish} 
-                    comments={comments} 
-                    dishId={dishId} 
-                    postComment={postComment} 
-                />
+                <CommentForm dishId={dishId} addComment={addComment} />
             </div>
         )
     }
@@ -247,59 +229,40 @@ class CommentForm extends Component {
 
         const dish = props.dish
         
-        if (props.isLoading) {
-            return (
-                <div className="container">
-                    <div className="row">
-                    </div>
-                </div>
-            );
-        }
-        else if (props.errMess){
-            return (
-                <div className="container">
-                    <div className="row">
-                        <h4>{props.errMess}</h4>
-                    </div>
-                </div>
-            );
-        }
-        else if (props.dish == null) {
+    
+        if (dish == null) {
             return (<div></div>);
         }
-        else if (props.dish != null) {
-            return (
-                <div className="container">
-                    <div className="row">
-                        <Breadcrumb>
-                            <BreadcrumbItem>
-                                <Link to="/menu">Menu</Link>
-                            </BreadcrumbItem>
-                            <BreadcrumbItem active>
-                                { props.dish.name }
-                            </BreadcrumbItem>
-                        </Breadcrumb>
-    
-                        <div className="col-12">
-                            <h3> {props.dish.menu}</h3>
-                            <hr />
-                        </div>
-                    </div>
-    
-                    <div className='row'>
-                        <RenderDish dish={ props.dish } />
-                        <RenderComments dish={ props.dish } comments={ props.comments } 
-                            postComment={ props.postComment }
-                            dishId={ props.dish.id }
-                        />
-                    </div>
-    
-    
-                </div>
-            )
-            
-        }
 
+        return (
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem>
+                            <Link to="/menu">Menu</Link>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem active>
+                            { props.dish.name }
+                        </BreadcrumbItem>
+                    </Breadcrumb>
+
+                    <div className="col-12">
+                        <h3> {props.dish.menu}</h3>
+                        <hr />
+                    </div>
+                </div>
+
+                <div className='row'>
+                    <RenderDish dish={ props.dish } />
+                    <RenderComments comments={props.comments}
+        addComment={props.addComment}
+        dishId={props.dish.id}
+      />
+                </div>
+
+
+            </div>
+        )
     }
 
 
