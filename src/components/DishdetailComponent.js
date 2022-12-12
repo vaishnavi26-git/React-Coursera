56dc3d7dc8938ceaed208dbf7c9f175689014698
Vaishnavi Kumari <vaishnavikumari26@gmail.com>
2022-12-12 22:44:31 +0530

    fetch server and error handling

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -8,6 +8,7 @@ import { Loading } from './LoadingComponent';
 
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
+import { baseUrl } from '../shared/baseUrl';
 
 
 
@@ -177,7 +178,7 @@ class CommentForm extends Component {
             return (
                 <div className='col-12 col-md-5 m-1'>
                     <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                         <CardBody>
                             <CardTitle> {dish.name}</CardTitle>
                             <CardText> {dish.description} </CardText>
